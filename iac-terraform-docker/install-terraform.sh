37295916b5f2769702695360bdd407c62efe5327
FMurkz <feliciamurkes@gmail.com>
2026-09-24 12:38:17 +0200

    revert

--- iac-terraform-docker/install-terraform.sh
+++ iac-terraform-docker/install-terraform.sh
@@ -2,7 +2,3 @@
 wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt update && apt install -y terraform
-echo ""
-echo "=========================================="
-echo "Setup complete -Terraform is ready to use."
-echo "=========================================="
