a1628457a8da53fb51d3170f84f06c207fa05d19
FMurkz <feliciamurkes@gmail.com>
2026-09-24 13:58:30 +0200

    update main.tf and nginx conf

--- iac-terraform-docker/assets/main.tf
+++ iac-terraform-docker/assets/main.tf
@@ -1,13 +1,29 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 4.2.0"
     }
   }
 }
 
 provider "docker" {}
+
+resource "docker_network" "app_net" {
+  name = "app_net"
+}
+
+resource "docker_image" "backend" {
+  name = "httpd:latest"
+}
+
+resource "docker_container" "backend" {
+  image = docker_image.backend.image_id
+  name  = "backend"
+  networks_advanced {
+    name = docker_network.app_net.name
+  }
+}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
@@ -20,5 +36,12 @@ resource "docker_container" "nginx" {
   ports {
     internal = 80
     external = 8000
+  }
+  networks_advanced {
+    name = docker_network.app_net.name
+  }
+  volumes {
+    host_path      = "/${path.cwd}/nginx.conf"
+    container_path = "/etc/nginx/conf.d/default.conf"
   }
 }
